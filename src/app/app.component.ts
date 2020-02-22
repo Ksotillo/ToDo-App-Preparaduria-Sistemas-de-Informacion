28446c0c858e5b89dba6033c87081744ec49550e
Kevin <ksotillo@gmail.com>
2020-02-22 11:07:04 -0400

    uncomment code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,8 @@ export class AppComponent {
 ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.newTodo.title = 'dsfasdads'
+    
+    // this.newTodo.title = 'dsfasdads'
     
 }
 
